867cfaaa31e9d8239db2b07b028cc02642fe4691
Kevin Cunnane <kcunnane@microsoft.com>
2019-05-07 13:32:15 -0700

    Use a helper package to simplify connect and query (#3)
    * Rewrite to use helper package
    
    * Bump package version

--- src/databaseCreator.ts
+++ src/databaseCreator.ts
@@ -8,40 +8,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as sqlops from 'sqlops';
-import { Deferred } from './promise';
-
+import { ConnectionContext } from 'azuredatastudio-dmpwrapper';
 const mssql = 'MSSQL';
 
 export class DatabaseCreator {
-    private counter = 0;
-    private connectionProvider: sqlops.ConnectionProvider;
-    private connectionTracker = new Map<string, Deferred<void>>();
 
     constructor() {
-        this.initConnectionProvider();
     }
-
-    /**
-     * Looks up connection provider if it doesn't exist, and registers to listen to connection complete events.
-     * This lets us wait on a connection we request to be completed before running a query against it.
-     *
-     */
-    private initConnectionProvider(): void {
-        this.connectionProvider = sqlops.dataprotocol.getProvider<sqlops.ConnectionProvider>(mssql, sqlops.DataProviderType.ConnectionProvider);
-        this.connectionProvider.registerOnConnectionComplete((summary: sqlops.ConnectionInfoSummary) => {
-            let trackedPromise = this.connectionTracker.get(summary.ownerUri);
-            if (trackedPromise) {
-                if (summary.connectionId) {
-                    // Having a connectionId indicates success (not sure why)
-                    trackedPromise.resolve();
-                } else {
-                    trackedPromise.reject(summary.errorMessage);
-                }
-                this.connectionTracker.delete(summary.ownerUri);
-            }
-        });
-    }
-
 
     /**
      * Prompts a user for database name and then connects to the existing DB and runs the create database command
@@ -50,8 +23,12 @@ export class DatabaseCreator {
      * @returns {Promise<void>}
      */
     public async createDatabase(context: sqlops.ObjectExplorerContext): Promise<void> {
-        // Make sure we have a connection
+        // Make sure we have a connection (handle command palette vs. context menu entry points)
         let connection = await this.lookupConnection(context);
+        if (!connection) {
+            vscode.window.showInformationMessage('Cannot create database as no active connection could be found');
+            return;
+        }
 
         // Prompt the user for a new database name
         let dbName = await vscode.window.showInputBox({ prompt: `Name of database to create on server ${connection.options['server']}`, validateInput: (value) => value && value.length > 124 ? 'Must be 124 chars or less' : undefined});
@@ -71,38 +48,47 @@ export class DatabaseCreator {
     }
 
     private async doCreateDatabase(operation: sqlops.BackgroundOperation, dbName: string, connection: sqlops.IConnectionProfile | sqlops.connection.Connection): Promise<void> {
-        let tempUri = `untitled:createdb${this.counter++}`;
-        let queryProvider = sqlops.dataprotocol.getProvider<sqlops.QueryProvider>(mssql, sqlops.DataProviderType.QueryProvider);
-        let connected = false;
+        
+        let connectionProvider = sqlops.dataprotocol.getProvider<sqlops.ConnectionProvider>(mssql, sqlops.DataProviderType.ConnectionProvider);
+        let connectionContext = new ConnectionContext(connectionProvider);
+
         try {
+            // 1. Connect to the server
             operation.updateStatus(sqlops.TaskStatus.InProgress, 'Connecting to database');
-            // Connect and execute a query. We use a temporary URI for this and dispose it on completion
-            connected = await this.doConnect(tempUri, connection);
+            let connected = await connectionContext.tryConnect(connection);
             if (!connected) {
-                vscode.window.showErrorMessage('Failed to create connection, canceling create database operation');
+                vscode.window.showErrorMessage('Failed to connect, canceling create database operation');
                 return;
             }
 
+            // 2. Run Create Database query 
             operation.updateStatus(sqlops.TaskStatus.InProgress, 'Executing create database query');
-            await this.runCreateDatabaseQuery(dbName, queryProvider, tempUri);
-
-            // Notify on success
+            let query = `BEGIN TRY
+    CREATE DATABASE [${dbName.replace(/]/g , "]]")}]
+    SELECT 1 AS NoError
+END TRY
+BEGIN CATCH
+    SELECT ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
+`;
+            let result = await connectionContext.runQueryAndReturn(query);
+            if (result.columnInfo[0].columnName === 'ErrorMessage') {
+                throw new Error(result.rows[0][0].displayValue);
+            }
+            
+            // 3. Notify on success
             let successMsg = `Database ${dbName} created. Refresh the Databases node to see it`;
             operation.updateStatus(sqlops.TaskStatus.Succeeded, successMsg);
             vscode.window.showInformationMessage(successMsg);
         } catch (error) {
-            // Notify on failure
+            // 4. Notify on failure
             let errorString = error instanceof Error ? error.message : error;
             let msg = 'Error adding database: ' + errorString;
             vscode.window.showErrorMessage(msg);
             operation.updateStatus(sqlops.TaskStatus.Failed, msg);
         } finally {
-            if (connected) {
-                // Disconnect and ignore any errors since a failure means the connection wasn't really established.
-                this.connectionProvider.disconnect(tempUri).then(success => undefined, fail => undefined);
-            }
+            connectionContext.dispose();
         }
-
     }
     
 
@@ -130,9 +116,6 @@ export class DatabaseCreator {
             }
         }
 
-        if (!connection) {
-            vscode.window.showInformationMessage('Cannot create database as no active connection could be found');return;
-        }
         return connection;
     }
     
@@ -145,53 +128,6 @@ export class DatabaseCreator {
         });
 
         return confirmed && confirmed.value === true;
-    }
-
-    /**
-     * This handles the async connection pattern used by Azure Data Studio. It sends a connection request and listens for a connection complete
-     * response by storing a lookup value in a map that'll be called back when the connection complete for the URI we use is completed.
-     * It'd be nice if there's was a cleaner "connectAndWait" method in the APIs so this kind of double-hop isn't needed, but this is the current
-     * implementation.
-     *
-     * @param {string} uri a unique URI for this connection. Could refer to an editor but we're using a semi-GUID based approach so we can track the connection
-     * @param {(sqlops.IConnectionProfile | sqlops.connection.Connection)} connection Information needed to connect
-     * @returns {Promise<boolean>} A Promise that resolves to true/false depending on if connection succeeds
-     */
-    private async doConnect(uri: string, connection: sqlops.IConnectionProfile | sqlops.connection.Connection): Promise<boolean> {
-        let deferred = new Deferred<void>();
-        this.connectionTracker.set(uri, deferred);
-
-        let connectRequested = await this.connectionProvider.connect(uri, connection);
-        if (!connectRequested) {
-            this.connectionTracker.delete(uri);
-            return false;
-        }
-        try {
-            // Wait on the connection to complete, or 15 seconds (mostly in case our logic is faulty)
-            await Promise.race([deferred.promise, this.errorOnTimeout(15000)]);
-        } catch (err) {
-            return false;
-        }
-        return true;
-    }
-
-    private async runCreateDatabaseQuery(dbName: string, queryProvider: sqlops.QueryProvider, tempUri: string) {
-        let query = `BEGIN TRY
-    CREATE DATABASE [${dbName.replace(/]/g , "]]")}]
-    SELECT 1 AS NoError
-END TRY
-BEGIN CATCH
-    SELECT ERROR_MESSAGE() AS ErrorMessage;
-END CATCH
-`;
-        let result = await queryProvider.runQueryAndReturn(tempUri, query);
-        if (result.columnInfo[0].columnName === 'ErrorMessage') {
-            throw new Error(result.rows[0][0].displayValue);
-        }
-    }
-
-    private errorOnTimeout(ms: number): Promise<void> {
-        return new Promise((resolve, reject) => setTimeout(reject, ms));
     }
 }
 
